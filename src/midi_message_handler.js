c791b2ed21bb52e5798833f6e6772cbe52091dc7
ryohey <info@codingcafe.jp>
2017-08-15 16:03:15 +0900

    Separate processMidiMessage from wml

---
+++ src/midi_message_handler.js
@@ -0,0 +1,102 @@
+export default class MidiMessageHandler {
+  /** @type {Array.<number>} */
+  RpnMsb = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  /** @type {Array.<number>} */
+  RpnLsb = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  /**
+   * @param {Array.<number>} message
+   */
+  processMidiMessage(message) {
+    /** @type {number} */
+    const channel = message[0] & 0x0f
+    /** @type {Synthesizer} */
+    const { synth } = this
+
+    if (!synth) {
+      return
+    }
+
+    switch (message[0] & 0xf0) {
+      case 0x80: // NoteOff: 8n kk vv
+        synth.noteOff(channel, message[1], message[2])
+        break
+      case 0x90: // NoteOn: 9n kk vv
+        if (message[2] > 0) {
+          synth.noteOn(channel, message[1], message[2])
+        } else {
+          synth.noteOff(channel, message[1], 0)
+        }
+        break
+      case 0xB0: // Control Change: Bn cc dd
+        switch (message[1]) {
+          case 0x06: // Data Entry: Bn 06 dd
+            switch (this.RpnMsb[channel]) {
+              case 0:
+                switch (this.RpnLsb[channel]) {
+                  case 0: // Pitch Bend Sensitivity
+                    synth.pitchBendSensitivity(channel, message[2])
+                    break
+                }
+                break
+            }
+            break
+          case 0x07: // Volume Change: Bn 07 dd
+            synth.volumeChange(channel, message[2])
+            break
+          case 0x0A: // Panpot Change: Bn 0A dd
+            synth.panpotChange(channel, message[2])
+            break
+          case 0x78: // All Sound Off: Bn 78 00
+            synth.allSoundOff(channel)
+            break
+          case 0x79: // Reset All Control: Bn 79 00
+            synth.resetAllControl(channel)
+            break
+          case 0x20: // BankSelect
+            //console.log("bankselect:", channel, message[2])
+            break
+          case 0x64: // RPN MSB
+            this.RpnMsb[channel] = message[2]
+            break
+          case 0x65: // RPN LSB
+            this.RpnLsb[channel] = message[2]
+            break
+          default:
+          // not supported
+        }
+        break
+      case 0xC0: // Program Change: Cn pp
+        synth.programChange(channel, message[1])
+        break
+      case 0xE0: // Pitch Bend
+        synth.pitchBend(channel, message[1], message[2])
+        break
+      case 0xf0: // System Exclusive Message
+        // ID number
+        switch (message[1]) {
+          case 0x7e: // non-realtime
+            // TODO
+            break
+          case 0x7f: // realtime
+            const device = message[2]
+            // sub ID 1
+            switch (message[3]) {
+              case 0x04: // device control
+                // sub ID 2
+                switch (message[4]) {
+                  case 0x01: // master volume
+                    synth.setMasterVolume(message[5] + (message[6] << 7))
+                    break
+                }
+                break
+            }
+            break
+        }
+        break
+      default: // not supported
+        break
+    }
+  }
+}
--- src/wml.js
+++ src/wml.js
@@ -1,21 +1,16 @@
 import Synthesizer from "./sound_font_synth"
+import MessageHandler from "./message_handler"
 
 /**
  * @constructor
  */
 const WebMidiLink = function() {
-  /** @type {Array.<number>} */
-  this.RpnMsb = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  /** @type {Array.<number>} */
-  this.RpnLsb = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  /** @type {boolean} */
-  this.ready;
-  /** @type {Synthesizer} */
-  this.synth;
   /** @type {function(ArrayBuffer)} */
   this.loadCallback;
   /** @type {Function} */
   this.messageHandler = this.onmessage.bind(this);
+
+  this.midiMessageHandler = new MidiMessageHandler();
 
   window.addEventListener('DOMContentLoaded', function() {
     this.ready = true;
@@ -73,6 +68,7 @@ WebMidiLink.prototype.loadSoundFont = function(input) {
   if (!this.synth) {
     synth = this.synth = new Synthesizer(input);
     document.body.appendChild(synth.drawSynth());
+    this.midiMessageHandler.synth = synth;
     synth.init();
     synth.start();
     window.addEventListener('message', this.messageHandler, false);
@@ -99,7 +95,7 @@ WebMidiLink.prototype.onmessage = function(ev) {
 
   switch (type) {
     case 'midi':
-      this.processMidiMessage(
+      this.midiMessageHandler.processMidiMessage(
         msg.map(function(hex) {
           return parseInt(hex, 16);
         })
@@ -134,97 +130,6 @@ WebMidiLink.prototype.onmessage = function(ev) {
  */
 WebMidiLink.prototype.setLoadCallback = function(callback) {
   this.loadCallback = callback;
-};
-
-/**
- * @param {Array.<number>} message
- */
-WebMidiLink.prototype.processMidiMessage = function(message) {
-  /** @type {number} */
-  var channel = message[0] & 0x0f;
-  /** @type {Synthesizer} */
-  var synth = this.synth;
-
-  switch (message[0] & 0xf0) {
-    case 0x80: // NoteOff: 8n kk vv
-      synth.noteOff(channel, message[1], message[2]);
-      break;
-    case 0x90: // NoteOn: 9n kk vv
-      if (message[2] > 0) {
-        synth.noteOn(channel, message[1], message[2]);
-      } else {
-        synth.noteOff(channel, message[1], 0);
-      }
-      break;
-    case 0xB0: // Control Change: Bn cc dd
-      switch (message[1]) {
-        case 0x06: // Data Entry: Bn 06 dd
-          switch (this.RpnMsb[channel]) {
-            case 0:
-              switch (this.RpnLsb[channel]) {
-                case 0: // Pitch Bend Sensitivity
-                  synth.pitchBendSensitivity(channel, message[2]);
-                  break;
-              }
-              break;
-          }
-          break;
-        case 0x07: // Volume Change: Bn 07 dd
-          synth.volumeChange(channel, message[2]);
-          break;
-        case 0x0A: // Panpot Change: Bn 0A dd
-          synth.panpotChange(channel, message[2]);
-          break;
-        case 0x78: // All Sound Off: Bn 78 00
-          synth.allSoundOff(channel);
-          break;
-        case 0x79: // Reset All Control: Bn 79 00
-          synth.resetAllControl(channel);
-          break;
-        case 0x20: // BankSelect
-          //console.log("bankselect:", channel, message[2]);
-          break;
-        case 0x64: // RPN MSB
-          this.RpnMsb[channel] = message[2];
-          break;
-        case 0x65: // RPN LSB
-          this.RpnLsb[channel] = message[2];
-          break;
-        default:
-        // not supported
-      }
-      break;
-    case 0xC0: // Program Change: Cn pp
-      synth.programChange(channel, message[1]);
-      break;
-    case 0xE0: // Pitch Bend
-      synth.pitchBend(channel, message[1], message[2]);
-      break;
-    case 0xf0: // System Exclusive Message
-      // ID number
-      switch (message[1]) {
-        case 0x7e: // non-realtime
-          // TODO
-          break;
-        case 0x7f: // realtime
-          var device = message[2];
-          // sub ID 1
-          switch (message[3]) {
-            case 0x04: // device control
-              // sub ID 2
-              switch (message[4]) {
-                case 0x01: // master volume
-                  synth.setMasterVolume(message[5] + (message[6] << 7));
-                  break;
-              }
-              break;
-          }
-          break;
-      }
-      break;
-    default: // not supported
-      break;
-  }
 };
 
 export default WebMidiLink
