bf8946f465657e8810cee813f9b8777454243538
ryohey <info@codingcafe.jp>
2017-12-28 00:24:50 +0900

    Rewrite to class

--- src/WebMidiLink.ts
+++ src/WebMidiLink.ts
@@ -2,137 +2,117 @@ import Synthesizer from "./Synthesizer"
 import View from "./View"
 import MidiMessageHandler from "./MidiMessageHandler"
 
-/**
- * @constructor
- */
-const WebMidiLink = function() {
-  /** @type {function(ArrayBuffer)} */
-  this.loadCallback
-  /** @type {Function} */
-  this.messageHandler = this.onmessage.bind(this)
+export default class WebMidiLink {
+  loadCallback: (ArrayBuffer) => void
+  midiMessageHandler: MidiMessageHandler
+  ready: boolean = false
+  synth: Synthesizer
+  view: View
 
-  this.midiMessageHandler = new MidiMessageHandler()
+  constructor() {
+    this.midiMessageHandler = new MidiMessageHandler()
 
-  window.addEventListener('DOMContentLoaded', function() {
-    this.ready = true
-  }.bind(this), false)
-}
-
-WebMidiLink.prototype.setup = function(url) {
-  if (!this.ready) {
-    window.addEventListener('DOMContentLoaded', function onload() {
-      window.removeEventListener('DOMContentLoaded', onload, false)
-      this.load(url)
+    window.addEventListener('DOMContentLoaded', function() {
+      this.ready = true
     }.bind(this), false)
-  } else {
-    this.load(url)
   }
-}
 
-WebMidiLink.prototype.load = function(url) {
-  /** @type {XMLHttpRequest} */
-  var xhr = new XMLHttpRequest()
-
-  xhr.open('GET', url, true)
-  xhr.responseType = 'arraybuffer'
-
-  xhr.addEventListener('load', function(ev) {
-    /** @type {XMLHttpRequest} */
-    var xhr = ev.target
-
-    this.onload(xhr.response)
-    if (typeof this.loadCallback === 'function') {
-      this.loadCallback(xhr.response)
+  setup(url) {
+    if (!this.ready) {
+      window.addEventListener('DOMContentLoaded', function onload() {
+        window.removeEventListener('DOMContentLoaded', onload, false)
+        this.load(url)
+      }.bind(this), false)
+    } else {
+      this.load(url)
     }
-  }.bind(this), false)
-
-  xhr.send()
-}
-
-/**
- * @param {ArrayBuffer} response
- */
-WebMidiLink.prototype.onload = function(response) {
-  /** @type {Uint8Array} */
-  var input = new Uint8Array(response)
-
-  this.loadSoundFont(input)
-}
-
-/**
- * @param {Uint8Array} input
- */
-WebMidiLink.prototype.loadSoundFont = function(input) {
-  /** @type {Synthesizer} */
-  var synth
-
-  if (!this.synth) {
-    const ctx = new AudioContext()
-    synth = this.synth = new Synthesizer(ctx)
-    synth.init()
-    synth.refreshInstruments(input)
-    var view = this.view = new View()
-    document.body.appendChild(view.draw(synth))
-    this.midiMessageHandler.synth = synth
-    window.addEventListener('message', this.messageHandler, false)
-  } else {
-    synth = this.synth
-    synth.refreshInstruments(input)
   }
 
-  // link ready
-  if (window.opener) {
-    window.opener.postMessage("link,ready", '*')
-  } else if (window.parent !== window) {
-    window.parent.postMessage("link,ready", '*')
-  }
-}
+  load(url) {
+    const xhr = new XMLHttpRequest()
 
-/**
- * @param {Event} ev
- */
-WebMidiLink.prototype.onmessage = function(ev) {
-  var msg = ev.data.split(',')
-  var type = msg.shift()
-  var command
+    xhr.open('GET', url, true)
+    xhr.responseType = 'arraybuffer'
 
-  switch (type) {
-    case 'midi':
-      this.midiMessageHandler.processMidiMessage(
-        msg.map(function(hex) {
-          return parseInt(hex, 16)
-        })
-      )
-      break
-    case 'link':
-      command = msg.shift()
-      switch (command) {
-        case 'reqpatch':
-          // TODO: dummy data
-          if (window.opener) {
-            window.opener.postMessage("link,patch", '*')
-          } else if (window.parent !== window) {
-            window.parent.postMessage("link,patch", '*')
-          }
-          break
-        case 'setpatch':
-          // TODO: NOP
-          break
-        default:
-          console.error('unknown link message:', command)
-          break
+    xhr.addEventListener('load', function(ev) {
+      const xhr = ev.target as XMLHttpRequest
+
+      this.onload(xhr.response)
+      if (typeof this.loadCallback === 'function') {
+        this.loadCallback(xhr.response)
       }
-      break
-    default:
-      console.error('unknown message type')
+    }.bind(this), false)
+
+    xhr.send()
+  }
+
+  onload(response: ArrayBuffer) {
+    const input = new Uint8Array(response)
+    this.loadSoundFont(input)
+  }
+
+  loadSoundFont(input: Uint8Array) {
+    let synth: Synthesizer
+
+    if (!this.synth) {
+      const ctx = new AudioContext()
+      synth = this.synth = new Synthesizer(ctx)
+      synth.init()
+      synth.refreshInstruments(input)
+      const view = this.view = new View()
+      document.body.appendChild(view.draw(synth))
+      this.midiMessageHandler.synth = synth
+      window.addEventListener('message', this.onmessage.bind(this), false)
+    } else {
+      synth = this.synth
+      synth.refreshInstruments(input)
+    }
+
+    // link ready
+    if (window.opener) {
+      window.opener.postMessage("link,ready", '*')
+    } else if (window.parent !== window) {
+      window.parent.postMessage("link,ready", '*')
+    }
+  }
+
+  onmessage(ev: MessageEvent) {
+    const msg = ev.data.split(',')
+    const type = msg.shift()
+
+    switch (type) {
+      case 'midi':
+        this.midiMessageHandler.processMidiMessage(
+          msg.map(function(hex) {
+            return parseInt(hex, 16)
+          })
+        )
+        break
+      case 'link':
+        const command = msg.shift()
+        switch (command) {
+          case 'reqpatch':
+            // TODO: dummy data
+            if (window.opener) {
+              window.opener.postMessage("link,patch", '*')
+            } else if (window.parent !== window) {
+              window.parent.postMessage("link,patch", '*')
+            }
+            break
+          case 'setpatch':
+            // TODO: NOP
+            break
+          default:
+            console.error('unknown link message:', command)
+            break
+        }
+        break
+      default:
+        console.error('unknown message type')
+    }
+  }
+
+  setLoadCallback(callback: (ArrayBuffer) => void) {
+    this.loadCallback = callback
   }
 }
-
-/**
- * @param {function(ArrayBuffer)} callback
- */
-WebMidiLink.prototype.setLoadCallback = function(callback) {
-  this.loadCallback = callback
-}
-
-export default WebMidiLink
