1bde0fa28dbe0913756dfe5425170ebe3738bcc3
ryohey <info@codingcafe.jp>
2017-12-27 23:10:52 +0900

    Fix Parser test

--- src/Parser.test.ts
+++ src/Parser.test.ts
@@ -1,13 +1,12 @@
 const fs = require("fs")
-import Parser from "./Parser.ts"
-import SoundFont from "./SoundFont.ts"
+import Parser from "./Parser"
 
 test('Parser', () => {
-  const input = fs.readFileSync("./fixture/clarinet_angel_pure_v1.1.sf2")
+  const input = fs.readFileSync("./fixture/TestSoundFont.sf2")
   const parser = new Parser(input)
   parser.parse()
   expect(parser.instrument.length).toBe(2)
-  expect(parser.sample.length).toBe(11)
+  expect(parser.sample.length).toBe(20)
   expect(parser.presetHeader.length).toBe(2)
-  expect(parser.presetHeader[0].presetName).toBe("Clarinet")
+  expect(parser.presetHeader[0].presetName).toBe("tr909")
 })
