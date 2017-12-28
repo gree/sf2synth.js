87daffbdaff1bce3d792ea63b375ec1b6c793c87
ryohey <info@codingcafe.jp>
2017-12-28 13:00:19 +0900

    Add tests

--- src/Parser.test.ts
+++ src/Parser.test.ts
@@ -1,11 +1,23 @@
 const fs = require("fs")
 import parse from "./Parser"
 
-test('Parser', () => {
+describe('Parser', () => {
   const input = fs.readFileSync("./fixture/TestSoundFont.sf2")
   const parser = parse(input)
-  expect(parser.instrument.length).toBe(1)
-  expect(parser.sample.length).toBe(19)
-  expect(parser.presetHeader.length).toBe(1)
-  expect(parser.presetHeader[0].presetName).toBe("tr909")
+  it("should parse instruments", () => {
+    expect(parser.instrument.length).toBe(1)
+    expect(parser.instrument[0].instrumentName).toBe("tr909")
+  })
+  it("should parse samples", () => {
+    expect(parser.sample.length).toBe(19)
+    expect(parser.sampleHeader.length).toBe(19)
+    expect(parser.sampleHeader[0].sampleName).toBe("bassdrum1")
+    expect(parser.sampleHeader[0].sampleRate).toBe(44100)
+    expect(parser.sampleHeader[0].originalPitch).toBe(76)
+    expect(parser.sampleHeader[0].pitchCorrection).toBe(6)
+  })
+  it("should parse presets", () => {
+    expect(parser.presetHeader.length).toBe(1)
+    expect(parser.presetHeader[0].presetName).toBe("tr909")
+  })
 })
